0db5264052c95afda1df282c5b0f5f9d257a4812
funakoshi-dev <70654005+funakoshi-dev@users.noreply.github.com>
2021-11-07 23:47:25 +0900

    [WIP]

--- lib/chat.dart
+++ lib/chat.dart
@@ -6,7 +6,7 @@ final _db = FirebaseFirestore.instance;
 final _auth = FirebaseAuth.instance;
 User _user;
 
-class ChatPage extends StatefulWidget{
+class ChatPage extends StatefulWidget {
   static const String id = 'chat_screen';
   final String title = 'Chat';
 
@@ -26,88 +26,111 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: null,
-        actions: <Widget>[
-          IconButton(
-              onPressed: (){
-                _auth.signOut();
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.close)
-          )
-        ],
-        title: Text(widget.title),
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            MessageStream(),
-            Container(
-              child:Row(
+        appBar: AppBar(
+          leading: null,
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {
+                  _auth.signOut();
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.close))
+          ],
+          title: Text(widget.title),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              MessageStream(),
+              Container(
+                  child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Expanded(
                       child: TextField(
-                        controller: messageTextController,
-                        onChanged: (value){
-                          messageText = value;
-                        },
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 20.0
-                          ),
-                          hintText: 'Type your message here...',
-                          border: InputBorder.none,
-                        ),
-                      )
-                  ),
+                    controller: messageTextController,
+                    onChanged: (value) {
+                      messageText = value;
+                    },
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      hintText: 'Type your message here...',
+                      border: InputBorder.none,
+                    ),
+                  )),
                   TextButton(
-                      onPressed: (){
-                        messageTextController.clear();
-                        _db.collection('messages').add(
-                          {
-                            'text': messageText,
-                            'sender': _user.email,
-                            'time': FieldValue.serverTimestamp(),
-                          }
-                        );
-                      },
-                      child: Text(
+                    onPressed: () {
+                      messageTextController.clear();
+                      _db.collection('messages').add({
+                        'text': messageText,
+                        'sender': _user.email,
+                        'time': FieldValue.serverTimestamp(),
+                      });
+                    },
+                    child: Text(
                       'Send',
-                        style: TextStyle(
-                          color: Colors.lightBlueAccent,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
+                      style: TextStyle(
+                        color: Colors.lightBlueAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
                       ),
+                    ),
                   )
                 ],
-              )
-            )
-          ],
-        ),
-      )
-    );
+              ))
+            ],
+          ),
+        ));
   }
 }
 
-class MessageStream extends StatelessWidget{
+class MessageStream extends StatelessWidget {
   @override
-  Widget build(BuildContext context){}
+  Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream _db
-        .collection('messages')
-        .orderBy('time', descending:true)
-        .limit(50)
-        .snapshots(),
-      builder: (context, snapshot){}
-        if(!snapshot.hasData){}
-        
-      )
+      stream: _db
+          .collection('messages')
+          .orderBy('time', descending: true)
+          .limit(50)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+
+        final messages = snapshot.data.docs;
+        List<MessageLine> messageLines = [];
+        for (var message in messages) {
+          final Map<String, dynamic> doc = message.data();
+          final messageText = doc['text'];
+          final messageSender = doc['sender'];
+
+          final messageLine = MessageLine(
+            text: messageText,
+            sender: messageSender,
+            inMine: _user.email == messageSender,
+          );
+
+          messageLines.add(messageLine);
+        }
+
+        return Expanded(
+            child: ListView(
+          reverse: true,
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          children: messageLines,
+        ));
+      },
+    );
+  }
 }
